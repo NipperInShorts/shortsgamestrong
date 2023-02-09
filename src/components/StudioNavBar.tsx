a963dbb5bace186b9731638ea04eb67e0ab3d18f
Justin Nipper <justin.nipper@me.com>
2023-02-09 09:08:27 -0600

    Updates Sanity navbar styling to go back home

--- src/components/StudioNavBar.tsx
+++ src/components/StudioNavBar.tsx
@@ -5,13 +5,16 @@ import { NavbarProps } from "sanity";
 export default function StudioNavBar(props: NavbarProps) {
   return (
     <div>
-      <Link
-        href={"/"}
-        className="text-[var(--brand-primary)] flex items-center"
-      >
-        Back to Site
-      </Link>
-      <>{props.renderDefault(props)}</>
+      <div className="flex items-center justify-between p-5">
+        <Link
+          href={"/"}
+          className="text-[var(--brand-primary)] flex items-center"
+        >
+          <ArrowUturnLeftIcon className="h-6 w-6 test-[var(--brand-primary)] mr-2" />
+          Back to Site
+        </Link>
+      </div>
+      {props.renderDefault(props)}
     </div>
   );
 }
