d6d6fcc354c4b2339c0c2ef9f17ec02a57ec3b1e
Justin Nipper <justin.nipper@me.com>
2025-10-10 11:15:37 -0400

    updates text and adds contact me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,21 @@ import { apps } from "./appList";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex flex-col">
+      <main className="flex flex-col gap-[32px] row-start-2 sm:items-start">
+        <div className="flex flex-col items-center sm:items-start">
           <h1 className="text-xl md:text-2xl mb-1 font-medium">
-            Justin Nipper
+            Hi, I&#39;m Justin Nipper
           </h1>
-          <p className="my-2">
-            I&#39;m a developer and engineering leader who builds mobile apps
-            and video games.
+          <p className="my-2 text-center">
+            Developer and engineering leader who builds mobile apps and video
+            games.
           </p>
+          <p className="my-2">Want to work together?</p>
+          <a
+            href="mailto:nipper@shortsgamestrong.com"
+            className="my-2 rounded-lg bg-purple-800 px-6 py-3 text-lg font-medium text-white hover:bg-purple-500 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:border-purple-700 dark:bg-purple-800 dark:hover:bg-purple-700 dark:focus:ring-purple-700">
+            Contact Me
+          </a>
           <h2 className="mt-4 text-xl md:text-2xl mb-1 font-medium">
             Some things I&#39;ve built
           </h2>
